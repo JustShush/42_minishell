0cc1367e833ad5b3bfa02848f7be18946a644611
JustShush <marquesdinis.p@gmail.com>
2023-12-01 16:45:20 +0000

    better comment

--- src/frees.c
+++ src/frees.c
@@ -6,7 +6,7 @@
 /*   By: dimarque <dimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:04:11 by dimarque          #+#    #+#             */
-/*   Updated: 2023/12/01 15:33:42 by dimarque         ###   ########.fr       */
+/*   Updated: 2023/12/01 17:24:24 by dimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,7 +59,7 @@ void	free_ms(t_minishell *ms)
 		free(ms->prompt);
 	if (ms->input)
 		free(ms->input);
-	printf("%sSEG_FAULT%s of main_arr when trying to free main_arr????\n", BRED, RESET);
+	printf("%sSEG_FAULT%s of main_arr when signal_D \n", BRED, RESET);
 	if (ms->main_arr)
 		free_arr(ms->main_arr);
 	printf("%sSEG_FAULT SOLVED%s of main_arr in frees.c!\n", BGREEN, RESET);
