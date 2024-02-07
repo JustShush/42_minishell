9b3a86ec7c519a84a7dccaadb2abcfcd0ad5b455
JustShush <marquesdinis.p@gmail.com>
2024-02-07 12:44:05 +0000

    forgot to norm the split file mb

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dimarque <dimarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 14:33:27 by dimarque          #+#    #+#             */
-/*   Updated: 2024/02/07 12:04:39 by dimarque         ###   ########.fr       */
+/*   Updated: 2024/02/07 12:43:40 by dimarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,10 @@
  * @param c The delimiter character used to separate substrings
  * @param list (optional) An array to store the substrings, if provided
  * @return The number of substrings found in the string
- * @note If 'list' is provided, the function also populates it with the substrings found in 'str'
- * @warning If 'list' is provided, ensure it has enough space to store all the substrings
+ * @note If 'list' is provided, the function also populates it with the
+ * substrings found in 'str'
+ * @warning If 'list' is provided, ensure it has enough space to store all the
+ * substrings
  */
 static int	ft_nstr(char const *str, char c, char **list)
 {
