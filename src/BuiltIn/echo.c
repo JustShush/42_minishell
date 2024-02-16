706082954f8d9e845905c7a90d3fe86cfac98dda
Maria De Avelar Galvao Santos Lima <mde-avel@c2r9s3.42lisboa.com>
2024-02-16 17:03:47 +0000

    leaks && echo_v4.2

--- src/BuiltIn/echo.c
+++ src/BuiltIn/echo.c
@@ -37,28 +37,64 @@ int	check_option(char *opt)
 	return (0);
 }
 
-void	command_echo(char **cmd_line, int flag, int i)
+//ft_printf("%snew_cmd:%s|%s|\n", PURPLE, RESET, cmd_line[i]);
+char	*quot_marks(char *cmd_line)
 {
-	if (flag)
+	size_t	len;
+	char	*new_cmd;
+
+	len = ft_strlen(cmd_line) - 1;
+	if (cmd_line[0] == '"' && cmd_line[len] == '"')
 	{
-		while (cmd_line[i])
-		{
-			ft_printf("%s", cmd_line[i]);
-			if (cmd_line[i + 1] != NULL)
-				ft_printf(" ");
-			i++;
-		}
+		new_cmd = ft_strtrim(cmd_line, "\"");
 	}
 	else
+		return (NULL);
+	return (new_cmd);
+}
+
+void	with_option(char **cmd_line, int i)
+{
+	char	*new_cmd;
+
+	ft_printf("%swith_option:%s\n", PURPLE, RESET);
+	while (cmd_line[i])
 	{
-		i = 1;
-		while (cmd_line[i])
+		new_cmd = quot_marks(cmd_line[i]);
+		if (new_cmd == NULL)
+			ft_printf("%s", cmd_line[i]);
+		else
 		{
-			ft_printf("%s ", cmd_line[i]);
-			i++;
+			ft_printf("%s", new_cmd);
+			free(new_cmd);
 		}
-		ft_printf("\n");
+		if (cmd_line[i + 1] != NULL)
+			ft_printf(" ");
+		i++;
 	}
+}
+
+//echo hello "banana    minion gosta"
+void	no_option(char **cmd_line)
+{
+	int i;
+	char	*new_cmd;
+
+	i = 1;
+	ft_printf("%sno_option:%s\n", BLUE, RESET);
+	while (cmd_line[i])
+	{
+		new_cmd = quot_marks(cmd_line[i]);
+		if (new_cmd == NULL)
+			ft_printf("%s ", cmd_line[i]);
+		else
+		{
+			ft_printf("%s ", new_cmd);
+			free(new_cmd);
+		}
+		i++;
+	}
+	ft_printf("\n");
 }
 
 void	echo(char **cmd_line)
@@ -70,10 +106,13 @@ void	echo(char **cmd_line)
 		return ;
 	flag = 0;
 	i = 1;
-	while (check_option(cmd_line[i]) == 1)
+	if (check_option(cmd_line[i]) == 1)
 	{
 		flag = 1;
 		i++;
 	}
-	command_echo(cmd_line, flag, i);
+	if (flag)
+		with_option(cmd_line, i);
+	else
+		no_option(cmd_line);
 }
